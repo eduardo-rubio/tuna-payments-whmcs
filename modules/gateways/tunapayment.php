1d74b88afcb26cf593511881c50f643f532cb2e0
eduardo-rubio <eduardo.rubio@gmail.com>
2023-08-25 09:29:38 -0300

    Error 415

--- modules/gateways/tunapayment.php
+++ modules/gateways/tunapayment.php
@@ -266,7 +266,7 @@ function tunapayment_capture($params)
     $ch = curl_init();
     curl_setopt($ch, CURLOPT_URL, $paymentUrl);
     curl_setopt($ch, CURLOPT_POST, true);
-    curl_setopt($ch, CURLOPT_HTTPHEADER, $postheader);
+    curl_setopt($ch, CURLOPT_HTTPHEADER, http_build_query($postheader));
     curl_setopt($ch, CURLOPT_POSTFIELDS, http_build_query($postfields));
     curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
     $response = curl_exec($ch);
@@ -464,7 +464,7 @@ function tunapayment_refund($params)
     $ch = curl_init();
     curl_setopt($ch, CURLOPT_URL, $cancelUrl);
     curl_setopt($ch, CURLOPT_POST, true);
-    curl_setopt($ch, CURLOPT_HTTPHEADER, $postheader);
+    curl_setopt($ch, CURLOPT_HTTPHEADER, http_build_query($postheader));
     curl_setopt($ch, CURLOPT_POSTFIELDS, http_build_query($postfields));
     curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
     $response = curl_exec($ch);
